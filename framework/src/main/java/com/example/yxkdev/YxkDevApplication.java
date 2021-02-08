8fbd704126f220f98636849adf09f8f7bd8e90e7
yxk <yingxk@ngarihealth.com>
2021-02-08 10:59:32 +0800

    init

---
+++ framework/src/main/java/com/example/yxkdev/YxkDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.yxkdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YxkDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YxkDevApplication.class, args);
+    }
+
+}
---
+++ framework/src/test/java/com/example/yxkdev/YxkDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yxkdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YxkDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
